275b9baff74a800fc22f4a510454c98255c9896d
Egor Nikulin <egornik62@gmail.com>
2026-05-03 14:14:35 +0300

    fix: profile add and save hide logic

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -37,7 +37,7 @@ export const Profile: FC = () => {
       updateUser({
         name: formValue.name,
         email: formValue.email,
-        password: formValue.password || undefined
+        password: formValue.password
       })
     );
   };
